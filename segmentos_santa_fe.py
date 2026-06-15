cccd21af96c53986b64eba6840a29aa6845c8454
Alonso <alonsomoreco@icloud.com>
2026-06-14 19:06:28 -0700

    Primer commit

---
+++ segmentos_santa_fe.py
@@ -0,0 +1,17 @@
+SEGMENTOS = [
+    {
+        "nombre": "Vasco_de_Quiroga_EO",
+        "origen":  {"lat": 19.3601, "lng": -99.2601},
+        "destino": {"lat": 19.3598, "lng": -99.2401},
+    },
+    {
+        "nombre": "Periferico_Sur_NS",
+        "origen":  {"lat": 19.3720, "lng": -99.2510},
+        "destino": {"lat": 19.3520, "lng": -99.2510},
+    },
+    {
+        "nombre": "Presa_Tarango",
+        "origen":  {"lat": 19.3650, "lng": -99.2300},
+        "destino": {"lat": 19.3500, "lng": -99.2450},
+    },
+]
